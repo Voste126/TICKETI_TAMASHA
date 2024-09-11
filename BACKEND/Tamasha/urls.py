3d4a2b3b648bfc86c89fbfd83535ea74607c57f1
voste126 <steveaustine126@gmail.com>
2024-09-11 18:17:50 +0300

    FEAT:working on Deployment

--- BACKEND/Tamasha/urls.py
+++ BACKEND/Tamasha/urls.py
@@ -21,7 +21,7 @@ from tickets.views import TicketViewSet
 from accounts.views import SignUpView, LoginView
 from bookings.views import BookingsViewSet
 from payments.views import PaymentViewSet
-
+from django.views.generic import TemplateView
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -45,8 +45,10 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html')),
+
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
