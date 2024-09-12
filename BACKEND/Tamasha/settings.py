a1079083f291ce49bb4706ccd3a20e175d2b1ea4
voste126 <steveaustine126@gmail.com>
2024-09-12 08:55:51 +0300

    FIX:working on deployment

--- BACKEND/Tamasha/settings.py
+++ BACKEND/Tamasha/settings.py
@@ -93,16 +93,16 @@ WSGI_APPLICATION = 'Tamasha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': os.getenv('DB_NAME'),
-#         'USER': os.getenv('DB_USER'),
-#         'PASSWORD': os.getenv('DB_PASSWORD'),
-#         'HOST': os.getenv('DB_HOST'),
-#         'PORT': os.getenv('DB_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
 # Database configuration for Render deployment
 DATABASES = {
     'default': dj_database_url.config(
--- BACKEND/Tamasha/wsgi.py
+++ BACKEND/Tamasha/wsgi.py
@@ -1,11 +1,16 @@
-# wsgi.py
+"""
+WSGI config for Tamasha project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
 
 import os
+
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-from Tamasha.settings import BASE_DIR
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Tamasha.settings')
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
