b2dbef5fd891cd94e507d8625dbdc9458c4599f3
voste126 <steveaustine126@gmail.com>
2024-09-12 15:54:14 +0300

    FIX:working on deployment

--- FRONTEND/TamashaFrontend/src/components/EventList.js
+++ FRONTEND/TamashaFrontend/src/components/EventList.js
@@ -9,6 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 export default function EventList() {
   const [events, setEvents] = useState([]);
   const apiUrl = process.env.REACT_APP_API_URL;
+
   // Fetch events from the backend when the component mounts
   useEffect(() => {
     const fetchEvents = async () => {
@@ -19,25 +20,40 @@ export default function EventList() {
             'Authorization': `Bearer ${token}`,
           },
         });
-        setEvents(response.data); // Set the fetched events to the state
+
+        // Log the response data to confirm the format
+        console.log('Events response data:', response.data);
+
+        // Ensure data is an array
+        if (Array.isArray(response.data)) {
+          setEvents(response.data); // Set the fetched events to the state
+        } else {
+          toast.error('Unexpected response format from API.');
+        }
       } catch (error) {
-        toast.error('Failed to fetch events. Please to Login again.');
+        toast.error('Failed to fetch events. Please login again.');
+        console.error('Error fetching events:', error);
       }
     };
 
     fetchEvents();
-  }, []);
+  }, [apiUrl]);
 
   return (
     <>
       <Navbar />
       <ToastContainer />
       <MDBRow className='g-4'>
-        {events.map((event) => (
-          <MDBCol md='3' key={event.event_id}>
-            <EventCard event={event} /> {/* Pass event data as a prop */}
-          </MDBCol>
-        ))}
+        {/* Conditional rendering to ensure events is an array */}
+        {Array.isArray(events) && events.length > 0 ? (
+          events.map((event) => (
+            <MDBCol md='3' key={event.event_id}>
+              <EventCard event={event} /> {/* Pass event data as a prop */}
+            </MDBCol>
+          ))
+        ) : (
+          <p>No events available</p>
+        )}
       </MDBRow>
     </>
   );
